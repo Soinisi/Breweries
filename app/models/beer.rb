4b672d83bafa8ec8ed32a6fe4e91a51fc7447cc6
Simo Soininen <simo.s.soininen@helsinki.fi>
2016-01-31 23:41:25 +0200

    toinen viikko

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,19 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
+  has_many :ratings
+
+	def average_rating
+ 	#sum = 0.0
+ 	#if self.ratings.count != 0 
+ 		
+ 	#	self.ratings.each do |rating|
+ 	#		sum = sum + rating.score
+ 	#	end
+ 	#end
+ 	#
+ 	#return "#{sum / self.ratings.count}"
+ #end
+	return self.ratings.inject(0.0){|sum, rating| sum + rating.score } / self.ratings.count
+	end
+
 end
