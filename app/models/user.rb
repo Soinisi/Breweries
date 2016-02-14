d68a8049507c6b9d0f53270ccac9090ae9a91c65
Simo Soininen <simo.s.soininen@helsinki.fi>
2016-02-14 02:27:18 +0200

    viikko3.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ActiveRecord::Base
 include RatingAverage
+has_secure_password
  
 validates :username, uniqueness: true
 validates :username, length: {minimum: 3, maximum: 15}
- 
+validates :password, length: { minimum: 4 },
+                       format: {
+                          with: /\d.*[A-Z]|[A-Z].*\d/,
+                          message: "has to contain one number and one upper case letter"
+                       } 
 
 has_many :ratings, dependent: :destroy   # käyttäjällä on monta ratingia
 has_many :beers, through: :ratings
